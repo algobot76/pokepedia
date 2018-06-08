7e043daa2a01e9df9374d671506b4f7311798b12
Kaitian Xie <xkaitian@gmail.com>
2018-06-08 15:22:37 +0800

    Fix an import

--- src/components/FixedBar.js
+++ src/components/FixedBar.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import SearchBar from './TopBar/SearchBar/index';
+import SearchBar from './SearchBar';
 
 type Props = {
   title?: string,
