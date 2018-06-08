f277bcb7530aaaa5c4b975b631a32b09c2a29aee
Kaitian Xie <xkaitian@gmail.com>
2018-06-08 16:51:40 +0800

    Use Display in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import FixedBar from './FixedBar';
+import Display from './Display';
 
 class App extends Component {
   render() {
     return (
       <div>
         <FixedBar title="Welcome to Pokepdeia" />
+        <Display />
       </div>
     );
   }
