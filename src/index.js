abda0e9b8ca434f4f4aa65870c7f79d0fb35a214
Kaitian Xie <xkaitian@gmail.com>
2018-06-23 12:03:11 +0800

    Use redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 import './index.css';
 import App from './containers/App';
@@ -14,7 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 render(
   <Provider store={store}>
