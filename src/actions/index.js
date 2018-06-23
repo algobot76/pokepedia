4db596d10426e2e9e5f1e07d7275d3ce5e152f27
Kaitian Xie <xkaitian@gmail.com>
2018-06-23 11:25:23 +0800

    Make fetchCount work properly and use in App

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,9 +15,9 @@ export const requestCount = () => ({
   type: REQUEST_COUNT
 });
 
-export const receiveCount = json => ({
+export const receiveCount = res => ({
   type: RECEIVE_COUNT,
-  count: json.count
+  count: res.data.count
 });
 
 export const requestPokemon = () => ({
@@ -29,21 +29,9 @@ export const receivePokemon = json => ({
   data: json.results
 });
 
-const fetchCount = () => dispatch => {
+export const fetchCount = () => dispatch => {
   dispatch(requestCount());
   return pokeapi
     .get('pokemon')
-    .then(dispatch(response => receiveCount(response.data.count)));
-};
-
-const shouldFetchCount = state => {
-  const count = state.count;
-
-  return count === 0;
-};
-
-export const fetchCountIfNeeded = () => (dispatch, getState) => {
-  if (shouldFetchCount(getState())) {
-    return dispatch(fetchCount());
-  }
+    .then(response => dispatch(receiveCount(response)));
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import FixedBar from '../components/FixedBar';
 import Display from '../components/Display';
+import { fetchCount } from '../actions';
 
 class App extends Component {
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchCount());
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +20,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { count } = state;
+  return count;
+};
+
+export default connect(mapStateToProps)(App);
